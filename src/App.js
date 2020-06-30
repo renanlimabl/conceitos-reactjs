473f0d467e0d2f4df04e1961219dfc9d796faf4c
renanlimabl <renanlimabl@gmail.com>
2020-06-30 03:59:46 -0300

    Adiciona app com hooks useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repo, setRepo] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepo(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+    const newRepo = {
+      url: 'https://github.com/axios/axios',
+      title: 'Desafio ReactJS',
+      techs: ['ReactJS', 'NodeJS']
+    }
+
+    const response = await api.post('/repositories', newRepo)
+
+    setRepo(oldRepo => [...oldRepo, response.data])
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await api.delete(`/repositories/${id}`)
+    setRepo(repo.filter(rep => rep.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repo.map(item => (
+          <li key={item.id}>
+            {item.title}
+            <button onClick={() => handleRemoveRepository(item.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
